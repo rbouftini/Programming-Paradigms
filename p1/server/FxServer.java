0476197d42acc82580a75d896e8987d6bc879008
Rayane Bouftini <bouftinirayane@gmail.com>
2025-02-17 13:13:54 +0100

    adding download option from remote server

--- p1/server/FxServer.java
+++ p1/server/FxServer.java
@@ -1,16 +1,34 @@
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.DataOutputStream;
+import java.io.*;
 
 public class FxServer {
-    public static final int port = 5000;  
-    public static void main(String[] args) throws Exception{
-        // Server needs to request a port number from the OS through the Socket API
-        try(ServerSocket ss = new ServerSocket(port)){ // this throws an expection so I need to throw it or handle it
-            while(true){
-                try(Socket socket = ss.accept()){ //blocking call, listens forever
-                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                    out.write("Hello World!".getBytes());
+    public static final int PORT = 4000;
+    public static void main(String[] args) throws Exception {
+        try (ServerSocket ss = new ServerSocket(PORT)){
+            while (true) {
+                try(Socket socket = ss.accept()){
+                    InputStream in = socket.getInputStream();
+                    OutputStream out = socket.getOutputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    System.out.println("Client connected ");
+                    String header = reader.readLine();
+                    System.out.println("Header received is" + header);
+                    String[] tokens = header.split(" ");
+                    if (tokens[0].equals("download")){
+                        try(FileInputStream fileIn = new FileInputStream(tokens[1])){
+                            int file_size = fileIn.available();
+                            header = "OK " + file_size + "\n";
+                            out.write(header.getBytes());
+                            byte[] bytes = new byte[file_size];
+                            fileIn.read(bytes);
+                            out.write(bytes); 
+                        }
+                        catch(Exception e){
+                            System.out.println("Not found");
+                        }
+                    }
+                        
                 }
             }
         }
